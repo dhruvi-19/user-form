d8741ba3b6257fcdab9da10ecdee3532bca52d28
Dhruvi Bhuva <dhruvi.b@prometteur.in>
2024-07-18 17:06:00 +0530

    made common Button component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,55 @@ import Switch from "react-switch";
 import Button from "./Button";
 
 class App extends React.Component {
-  state = { loginMode: false };
+  state = {
+    loginMode: false,
+    email: "",
+    password: "",
+    confirmPassword: "",
+    message: "",
+  };
 
   handleModeChange = (checked) => {
     this.setState((prevState) => ({ loginMode: !prevState.loginMode }));
   };
 
   handleEmailChange = (event) => {
-    console.log(event);
+    this.setState({ email: event.target.value });
+  };
+
+  handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  handleConfirmPasswordChange = (event) => {
+    this.setState({ confirmPassword: event.target.value });
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const { loginMode, email, password, confirmPassword } = this.state;
+
+    const validEmail = email.length > 0;
+    const validPassword = password.length > 0;
+    const validConfirmPassword = confirmPassword.length > 0;
+
+    if (loginMode && validEmail && validPassword) {
+      this.setState({ message: "You are now logged in." });
+      return;
+    }
+
+    const passwordMatch = password === confirmPassword;
+
+    if (validEmail && validPassword && validConfirmPassword && passwordMatch) {
+      this.setState({ message: "Thank you for signing up!" });
+    } else {
+      this.setState({ message: "Passwords do not match" });
+    }
   };
 
   render() {
-    const { loginMode } = this.state;
+    const { loginMode, email, password, confirmPassword, message } = this.state;
     const text = loginMode ? "Login into Account" : "Register";
     const greeting = loginMode ? "Welcome back" : "I'm glad you're here";
 
@@ -35,13 +72,14 @@ class App extends React.Component {
 
           <h1 className="mb-6 text-center text-3xl font-bold">{greeting}</h1>
 
-          <form>
+          <form onSubmit={this.handleFormSubmit}>
             <label htmlFor="email">Email Address</label>
             <input
               id="email"
               type="text"
               className="h-8 w-full rounded border border-solid border-gray-300 px-3 py-5 mb-4"
               onChange={this.handleEmailChange}
+              value={email}
             />
 
             <label htmlFor="password">Password</label>
@@ -49,24 +87,31 @@ class App extends React.Component {
               id="password"
               type="password"
               className="h-8 w-full rounded border border-solid border-gray-300 px-3 py-5 mb-4"
+              onChange={this.handlePasswordChange}
+              value={password}
             />
 
             {!loginMode && (
               <>
                 <label htmlFor="confirmPassword">Confirm Password</label>
                 <input
-                  id="password"
+                  id="confirmPassword"
                   type="password"
                   className="h-8 w-full rounded border border-solid border-gray-300 px-3 py-5 mb-4"
+                  onChange={this.handleConfirmPasswordChange}
+                  value={confirmPassword}
                 />
               </>
             )}
             <Button
               className=" w-full  border-red-500 hover:bg-red-100"
+              type="submit"
               onClick={this.handleClick}
             >
               {text}
             </Button>
+
+            {message && <p className="mt-2 text-center">{message}</p>}
           </form>
         </div>
       </div>
--- src/Button.jsx
+++ src/Button.jsx
@@ -2,19 +2,30 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class Button extends React.Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+    type: PropTypes.string,
+  };
+
+  static defaultProps = {
+    onClick: () => {},
+    type: "button",
+  };
+
   render() {
-    const { children, className } = this.props;
+    const { children, className, onClick, type } = this.props;
 
     const baseClasses = "h-10 rounded border-2 border-solid";
     const mergedClasses = `${baseClasses} ${className}`;
 
-    return <button className={mergedClasses}>{children}</button>;
+    return (
+      <button onClick={onClick} type={type} className={mergedClasses}>
+        {children}
+      </button>
+    );
   }
 }
-
-Button.propTypes = {
-  children: PropTypes.node.isRequired,
-  className: PropTypes.string.isRequired,
-};
 
 export default Button;
