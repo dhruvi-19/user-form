e7ac0496cb0791941f0c07a7b46d43aff9c47653
Dhruvi Bhuva <dhruvi.b@prometteur.in>
2024-07-18 17:27:44 +0530

    Added jest tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,9 @@
+describe("mathematical operations", () => {
+  it("adds numbers", () => {
+    expect(1 + 1).toBe(2);
+  });
 
+  it("subtracts numbers", () => {
+    expect(7 - 5).toBe(2);
+  });
+});
